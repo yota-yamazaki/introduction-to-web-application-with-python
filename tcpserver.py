83f0822399a0da7e498fb7335c7f1fe4730dbab0
yota-yamazaki <yota.yamazaki@rightcode.co.jp>
2025-11-09 15:12:35 +0900

    requestに対してapatchのレスポンスを返す

--- tcpserver.py
+++ tcpserver.py
@@ -33,7 +33,14 @@ class TCPServer:
             with open("server_recv.txt", "wb") as f:
                 f.write(request)
 
-            # 返事は特に返さず、通信を終了させる
+            # クライアントへ送信するレスポンスデータをファイルから取得する
+            with open("server_send.txt", "rb") as f:
+                response = f.read()
+
+            # クライアントへレスポンスを送信する
+            client_socket.send(response)
+
+            # 通信を終了させる
             client_socket.close()
 
         finally:
@@ -43,4 +50,3 @@ class TCPServer:
 if __name__ == '__main__':
     server = TCPServer()
     server.serve()
-
